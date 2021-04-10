8a0826822c5de7aed001de1ba4ad57b8b82367fe
Radoslaw Zamojski <radoslaw.zamojski@gmail.com>
2021-04-11 01:12:45 +0200

    create employees

--- travel_booking/travel_booking/MainForm.cs
+++ travel_booking/travel_booking/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using travel_booking.Models;
 
 namespace travel_booking
 {
@@ -15,6 +16,21 @@ namespace travel_booking
         public MainForm()
         {
             InitializeComponent();
+            GetEmployees();
         }
+
+        private List<Employee> GetEmployees()
+        {
+            return new List<Employee>()
+            {
+                new Employee(1, "Radosław", "Zamojski"),
+                new Employee(2, "Patryk", "Henrykowski"),
+                new Employee(3, "Halina", "Frąckowiak")
+
+            };
+        }
+
     }
 }
+
+
---
+++ travel_booking/travel_booking/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travel_booking.Models
+{
+    public class Employee
+    {
+        public Employee(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Password = "112233";
+            LoggedIn = false;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public bool LoggedIn { get; set; }
+        public string FullName
+        {
+            get
+            {
+                if (FirstName != null && LastName != null)
+                   return $"{FirstName} {LastName}";
+                else
+                    return "";
+            }
+        }
+    
+
+    }
+}
+    
